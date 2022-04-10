da420ba43d3b40e66f845bcce58c4c86f1d4d993
jg151195 <62825246+jg151195@users.noreply.github.com>
2022-04-10 15:41:50 -0300

    .

--- public/MongoCrud/public/connection.js
+++ public/MongoCrud/public/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const url = 'mongodb+srv://mongo:mongo@teste.j92cj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-//const url = 'mongodb://localhost:27017';
+//const url = 'mongodb+srv://mongo:mongo@teste.j92cj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const url = 'mongodb://localhost:27017';
 
 
 function connectDataBase() {
